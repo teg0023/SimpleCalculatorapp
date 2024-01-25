9e854a71868024e80bbac24bded908b6135be501
teg0023 <teg0023@auburn.edu>
2024-01-25 06:02:17 -0600

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculatorapp
+//
+//  Created by user248010 on 1/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
